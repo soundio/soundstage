a87ecb695db12ab7d0d9a326cb6a6a59b7e323b6
stephband <stephband@googlemail.com>
2015-09-09 06:07:50 +0200

    Improves flanger with control voltages for depth and delay

--- js/soundio.flange.js
+++ js/soundio.flange.js
@@ -56,6 +56,22 @@
 		return input;
 	}
 
+	function UnityNode(audio) {
+		var oscillator = audio.createOscillator();
+		var waveshaper = audio.createWaveShaper();
+
+		var curve = new Float32Array(2);
+		curve[0] = curve[1] = 1;
+
+		oscillator.type = 'square';
+		oscillator.connect(waveshaper);
+		oscillator.frequency.value = 100;
+		waveshaper.curve = curve;
+		oscillator.start();
+
+		return waveshaper;
+	}
+
 	function createStereoFlanger(audio, settings) {
 		var options = extend({}, defaults, settings);
 		var splitter = audio.createChannelSplitter(2);
@@ -74,23 +90,6 @@
 		var dryGain = audio.createGain();
 		var merger = audio.createChannelMerger(2);
 		var output = audio.createGain();
-
-		function destroy() {
-			splitter.disconnect()
-			input.disconnect()
-			fbMerger.disconnect()
-			fbSplitter.disconnect()
-			fb.disconnect()
-			speed.disconnect();
-			ldepth.disconnect();
-			rdepth.disconnect();
-			ldelay.disconnect();
-			rdelay.disconnect();
-			wetGain.disconnect();
-			dryGain.disconnect();
-			merger.disconnect();
-			output.disconnect();
-		}
 
 		ldepth.channelCountMode = rdepth.channelCountMode = 'explicit';
 		ldepth.channelCount = rdepth.channelCount = 1;
@@ -115,8 +114,8 @@
 		fbSplitter.connect(ldelay, 1);
 		fbSplitter.connect(rdelay, 0);
 
-		ldepth.gain.value = options.depth;
-		rdepth.gain.value = -options.depth;
+		ldepth.gain.value = 0;
+		rdepth.gain.value = 0;
 
 		speed.type = 'triangle';
 		speed.frequency.value = options.frequency;
@@ -139,31 +138,49 @@
 
 		speed.start(0);
 
+		var unityNode = UnityNode(audio);
+		var depthGain = audio.createGain();
+		depthGain.gain.value = options.depth;
+		unityNode.connect(depthGain);
+
+		var invert = audio.createGain();
+		invert.gain.value = -1;
+		
+		depthGain.connect(ldepth.gain);
+		depthGain.connect(invert);
+		invert.connect(rdepth.gain);
+
+		var delayGain = audio.createGain();
+		delayGain.gain.value = options.delay;
+		unityNode.connect(delayGain);
+		delayGain.connect(ldelay.delayTime);
+		delayGain.connect(rdelay.delayTime);
+
 		var flanger = AudioObject(audio, input, output, {
 			frequency: speed.frequency,
 			feedback: fb.gain,
+			depth: depthGain.gain,
 			dry: dryGain.gain,
 			wet: wetGain.gain,
-
-			depth: {
-				get: function() {
-					return ldepth.gain.value;
-				},
-				set: function(value) {
-					ldepth.gain.value = value;
-					rdepth.gain.value = -value;
-				}
-			},
-
-			delay: {
-				get: function() {
-					return ldelay.delayTime.value;
-				},
-				set: function(value) {
-					ldelay.delayTime.value = rdelay.delayTime.value = value;
-				}
-			}
+			delay: delayGain.gain
 		});
+
+		function destroy() {
+			splitter.disconnect()
+			input.disconnect()
+			fbMerger.disconnect()
+			fbSplitter.disconnect()
+			fb.disconnect()
+			speed.disconnect();
+			ldepth.disconnect();
+			rdepth.disconnect();
+			ldelay.disconnect();
+			rdelay.disconnect();
+			wetGain.disconnect();
+			dryGain.disconnect();
+			merger.disconnect();
+			output.disconnect();
+		}
 
 		Object.defineProperties(flanger, {
 			destroy: { value: destroy, writable: true }
