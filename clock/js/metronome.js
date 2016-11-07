80307928e137379e491411eb104b3fb2ea6b45f5
stephband <stephband@googlemail.com>
2016-11-07 12:18:05 +0100

    smthng

--- clock/js/metronome.js
+++ clock/js/metronome.js
@@ -6,9 +6,10 @@
 	var assign      = Object.assign;
 
 	var defaults = {
-		accent: 4,
+		tick:   4,
+		tock:   1,
 		note:   72,
-		tick:   {},
+		source: {},
 		events: [
 			[0, "meter", 1, 4]
 		]
@@ -20,35 +21,50 @@
 	// [time, "noteon", number, velocity]
 	// [time, "noteoff", number]
 
+	function createMeterStream(events) {
+		events.sort(Fn.compose(Fn.byGreater, Fn.get('0')));
+		return Fn(events);
+	}
 
-	function BeatSequence(metronome) {
-		var n = -metronome.tick;
+	function createTickSequence(tick, tock, note) {
+		var n = -tock;
 
 		return Fn(function beat() {
-			if (stream.status === "done") { return; }
-			var accent = n % metronome.accent === 0 ;
+			if (this.status === "done") { return; }
+			var isTick = n % tick === 0 ;
 			return [
-				(n = n + metronome.tick),
+				(n = n + tock),
 				"noteon",
-				metronome.note + (accent ? 5 : 0),
-				accent ? 1 : 0.5
+				note + (isTick ? 5 : 0),
+				isTick ? 1 : 0.5
 			] ;
 		});
 	}
 
-
 	function Metronome(audio, clock, options) {
 		var metronome = this;
 		var settings  = assign({}, defaults, options);
-
-		var tick      = new AudioObject.Tick(audio, settings.tick);
+		var source    = new AudioObject.Tick(audio, settings.synth);
 		var playing   = false;
-		var stream;
+
+		var types = {
+			'noteon': function(event) {
+				source.start(event[0], event[2], event[3]);
+			},
+			
+			'meter': function(event) {
+				ticks = createTickSequence(event[2], event[3]);
+				cuehead && cuehead.stop(event[0]);
+				cuehead = clock.play(event[0], ticks, schedule);
+			}
+		};
+
+		var ticks, cuehead;
 
 		function schedule(event) {
-			if (playing && event[1] === "noteon") {
-				tick.start(event[0], event[2], event[3]);
-			}
+			if (!playing) { return; }
+			if (!types[event[1]]) { return; }
+			types[event[1]](event);
 		}
 
 		Object.defineProperties(this, {
@@ -69,38 +85,38 @@
 			var beat = Math.ceil(time ? clock.beatAtTime(time) : clock.beat);
 			playing = true;
 
-			var stream = BeatSequence(this);
-			stream.stop = function() { stream.status = "done"; };
+			var meters = createMeterStream(this.events);
+			clock.play(beat, meters, schedule)
 
-			clock.play(beat, stream, schedule);
+			//var stream = createTickSequence(this.tick, this.tock, this.note);
+			//stream.stop = function() { stream.status = "done"; };
+			//cuehead = clock.play(beat, stream, schedule);
 		};
 
 		this.stop = function(time) {
 			time = time || audio.currentTime;
 			playing = false;
-			stream.stop();
+			//stream.stop();
 		};
 
-		this.accent    = settings.accent;
-		this.note      = settings.note;
-
-		// Todo: put metronome patterns in sequences
-		//this.sequences = [];
-		this.source = tick;
+		this.source = source;
 		this.events = new Collection(settings.events, { index: '0' });
+		this.tick   = settings.tick;
+		this.tock   = settings.tock;
+		this.note   = settings.note;
 
-		// Connect tick to the audio destination
-		AudioObject.getOutput(tick).connect(audio.destination);
+		// Connect source to the audio destination
+		AudioObject.getOutput(source).connect(audio.destination);
 		if (settings.playing) { this.start(); }
 	}
 
 	Object.defineProperties(Metronome.prototype, {
-		accent: {
+		tick: {
 			get: function() { return this.events[0][3]; },
 			set: function(n) { this.events[0][3] = n; }
 		},
 
-		tick: {
+		tock: {
 			get: function() { return this.events[0][2]; },
 			set: function(n) { this.events[0][2] = n; }
 		}
