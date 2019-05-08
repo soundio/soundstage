1b2890a80af862afb90cc899ddac4b0ca3b2ea81
stephband <stephband@googlemail.com>
2019-05-08 16:28:45 +0200

    Moves components out of project (into sound.io)

--- components/sparky/midi/fns.js
+++
@@ -1,9 +0,0 @@
-import { functions }  from '../../../../sparky/module.js';
-import { frameClass } from '../../../../dom/module.js';
-import * as MIDI from '../../../../midi/module.js';
-
-const flashClass = frameClass('flash');
-
-functions['midi-status'] = function(node, input) {
-    MIDI.on('', (e) => flashClass(node));
-};
