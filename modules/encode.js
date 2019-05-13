acb6eb0c5378a8bb6144bbaaa74bb60ce9d71303
stephband <stephband@googlemail.com>
2019-05-13 17:56:30 +0200

    Adds encoding module

---
+++ modules/encode.js
@@ -0,0 +1,110 @@
+
+// Adapted from
+// https://github.com/Jam3/audiobuffer-to-wav/blob/master/index.js
+//
+// published under the MIT license
+//
+// The MIT License (MIT) Copyright (c) 2015 Jam3
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+export function bufferToWav(buffer, opt) {
+    opt = opt || {}
+
+    var numChannels = buffer.numberOfChannels
+    var sampleRate = buffer.sampleRate
+    var format = opt.float32 ? 3 : 1
+    var bitDepth = format === 3 ? 32 : 16
+
+    var result
+    if (numChannels === 2) {
+        result = interleave(buffer.getChannelData(0), buffer.getChannelData(1))
+    } else {
+        result = buffer.getChannelData(0)
+    }
+
+    return encodeWAV(result, format, sampleRate, numChannels, bitDepth)
+}
+
+function encodeWAV (samples, format, sampleRate, numChannels, bitDepth) {
+    var bytesPerSample = bitDepth / 8
+    var blockAlign = numChannels * bytesPerSample
+
+    var buffer = new ArrayBuffer(44 + samples.length * bytesPerSample)
+    var view = new DataView(buffer)
+
+    /* RIFF identifier */
+    writeString(view, 0, 'RIFF')
+    /* RIFF chunk length */
+    view.setUint32(4, 36 + samples.length * bytesPerSample, true)
+    /* RIFF type */
+    writeString(view, 8, 'WAVE')
+    /* format chunk identifier */
+    writeString(view, 12, 'fmt ')
+    /* format chunk length */
+    view.setUint32(16, 16, true)
+    /* sample format (raw) */
+    view.setUint16(20, format, true)
+    /* channel count */
+    view.setUint16(22, numChannels, true)
+    /* sample rate */
+    view.setUint32(24, sampleRate, true)
+    /* byte rate (sample rate * block align) */
+    view.setUint32(28, sampleRate * blockAlign, true)
+    /* block align (channel count * bytes per sample) */
+    view.setUint16(32, blockAlign, true)
+    /* bits per sample */
+    view.setUint16(34, bitDepth, true)
+    /* data chunk identifier */
+    writeString(view, 36, 'data')
+    /* data chunk length */
+    view.setUint32(40, samples.length * bytesPerSample, true)
+
+    if (format === 1) { // Raw PCM
+        floatTo16BitPCM(view, 44, samples)
+    } else {
+        writeFloat32(view, 44, samples)
+    }
+
+    return buffer
+}
+
+function interleave (inputL, inputR) {
+    var length = inputL.length + inputR.length
+    var result = new Float32Array(length)
+
+    var index = 0
+    var inputIndex = 0
+
+    while (index < length) {
+        result[index++] = inputL[inputIndex]
+        result[index++] = inputR[inputIndex]
+        inputIndex++
+    }
+
+    return result
+}
+
+function writeFloat32 (output, offset, input) {
+    for (var i = 0; i < input.length; i++, offset += 4) {
+        output.setFloat32(offset, input[i], true)
+    }
+}
+
+function floatTo16BitPCM (output, offset, input) {
+    for (var i = 0; i < input.length; i++, offset += 2) {
+        var s = Math.max(-1, Math.min(1, input[i]))
+        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true)
+    }
+}
+
+function writeString (view, offset, string) {
+    for (var i = 0; i < string.length; i++) {
+        view.setUint8(offset + i, string.charCodeAt(i))
+    }
+}
