800aa50ccd9684736d00c5b0f848cad83afabbcd
stephband <stephband@googlemail.com>
2019-05-22 02:50:27 +0200

    Export duration functions

--- module.js
+++ module.js
@@ -17,3 +17,4 @@ export { transforms, parseValue } from './modules/transforms.js';
 export { automate, isAudioParam, getValueAtTime } from './modules/automate.js';
 export * from './modules/encode.js';
 export { requestBuffer } from './modules/request-buffer.js';
+export { getEventsDuration, getEventDuration } from './modules/events.js';
