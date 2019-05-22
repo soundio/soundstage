a8ee404a8a733c82ed22d3c30ce1458ce2c09157
stephband <stephband@googlemail.com>
2019-05-22 02:50:49 +0200

    Export duration functions

---
+++ modules/events.js
@@ -0,0 +1,8 @@
+export function getEventsDuration() {
+    return 4;
+    throw new Error('Import this from old');
+}
+
+export function getEventDuration() {
+    throw new Error('Import this from old');
+}
