38d833efbdb71057a90da9a3ff7cf1ac683260bf
Stephen <stephband@cruncher.ch>
2022-12-07 20:19:21 +0100

    Test

--- test.js
+++ test.js
@@ -4,7 +4,29 @@ console.log('TESTING TESTING 1 2 3');
 const { By, Builder } = require('selenium-webdriver');
 const { suite }       = require('selenium-webdriver/testing');
 const assert          = require("assert");
+const driver          = await new Builder().forBrowser('chrome').build();
 
+await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
+
+let title        = await driver.getTitle();
+assert.equal("Web form", title);
+
+await driver.manage().setTimeouts({ implicit: 500 });
+
+let textBox      = await driver.findElement(By.name('my-text'));
+let submitButton = await driver.findElement(By.css('button'));
+
+await textBox.sendKeys('Selenium');
+await submitButton.click();
+
+let message      = await driver.findElement(By.id('message'));
+let value        = await message.getText();
+assert.equal("Received!", value);
+
+await driver.quit();
+
+
+/*
 suite(function(env) {
     describe('First script', function() {
         let driver;
@@ -35,3 +57,4 @@ suite(function(env) {
         });
     });
 });
+*/
