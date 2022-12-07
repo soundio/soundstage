c669ec24e9e8a074f85305147431b5a0b9455c42
Stephen <stephband@cruncher.ch>
2022-12-07 21:12:37 +0100

    Test

--- .github/workflows/tests.js
+++ .github/workflows/tests.js
@@ -26,6 +26,6 @@ async function run(browser, url) {
     await driver.quit();
 }
 
-run('chrome', 'http://127.0.0.1:8000/soundstage/test.html');
+//run('chrome', 'http://127.0.0.1:8000/soundstage/test.html');
 run('safari', 'http://127.0.0.1:8000/soundstage/test.html');
-run('firefox', 'http://127.0.0.1:8000/soundstage/test.html');
+//run('firefox', 'http://127.0.0.1:8000/soundstage/test.html');
