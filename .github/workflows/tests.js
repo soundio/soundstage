71ca87e1ff1e1b3e3abd6da87f1446d76b150871
Stephen <stephband@cruncher.ch>
2022-12-09 03:48:38 +0100

    Tests

--- .github/workflows/tests.js
+++ .github/workflows/tests.js
@@ -51,12 +51,12 @@ async function run(browser, url) {
     // but that's not reliable.
     let n = 0;
     const interval = setInterval(async () => {
-        const result = await driver.findElement(By.id('result')).getText();
+        const result = await driver.findElement(By.id('result')).getAttribute("innerHTML");
 console.log('check', ++n, result);
         if (result) {
             clearInterval(interval);
             console.log('--- ' + browser + ' ---');
-            console.log(await driver.findElement(By.id('console')).getText());
+            console.log(await driver.findElement(By.id('console')).getAttribute("innerHTML"));
             console.log(result);
             console.log('--- ' + browser + ' ---');
             await driver.quit();
